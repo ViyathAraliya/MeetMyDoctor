00f59bb7a565e2e85f657bdf50b57d6748d190b9
viyath araliya <viyatharaliya@gmail.com>
2024-06-13 01:34:02 +0530

    feat(server-side): Add new field 'clinicSessions' to the model 'Room'

--- server/models/Room.js
+++ server/models/Room.js
@@ -1,6 +1,18 @@
 const mongoose=require('mongoose');
+const ClinicSession = require('./ClinicSession');
 
 const RoomSchema=new mongoose.Schema({
-    roomNumber:Number
+    roomNumber:{
+        type: Number,
+        required:true,
+        unique:true,
+       index:true
+        
+    },
+    clinicSessions:{
+        type: [ClinicSession],
+        unique: true
+    }
 })
+
 module.exports=mongoose.model('Room',RoomSchema);
