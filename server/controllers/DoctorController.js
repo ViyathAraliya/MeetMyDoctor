f365ab354a22ecc535868db2e2285dc96581e09b
viyath araliya <viyatharaliya@gmail.com>
2024-06-13 03:00:58 +0530

    refactor(server-side): Implement dto pattern at end point of the model 'Doctor'

--- server/controllers/DoctorController.js
+++ server/controllers/DoctorController.js
@@ -1,15 +1,22 @@
+const DoctorDto = require('../dtos/DoctorDto');
 const Doctor = require('../models/Doctor');
-//const mongoose = require('mongoose');
+
+
 
 const addDoctor = async (req, res) => {
-    
-   const requestedDoctor = req.body;
-   
 
+    const { name, contactNumber, whatsapp, email,
+        specialization, educationAbbrivation, generalSlotDuration } = req.body;
+
+    const doctorDto = new DoctorDto(name, contactNumber, whatsapp, email,
+        specialization, educationAbbrivation, generalSlotDuration);
+
+    const doctor = new Doctor({
+        name: doctorDto.name, contactNumber: doctorDto.contactNumber,
+        whatsapp: doctorDto.whatsapp, email: doctorDto.email, specialization: doctorDto.specialization,
+        educationAbbrivation: doctorDto.educationAbbrivation, generalSlotDuration: doctorDto.generalSlotDuration
+    })
     try {
-        const doctor=new Doctor(requestedDoctor);
-       
-
         await doctor.save();
         res.status(201).send(doctor);
 
@@ -20,6 +27,6 @@ const addDoctor = async (req, res) => {
 
 }
 
-module.exports={
+module.exports = {
     addDoctor
 }
