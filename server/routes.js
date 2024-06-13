ac892ab940e269bfa3d5c505673d785a32042d42
viyath araliya <viyatharaliya@gmail.com>
2024-06-13 08:41:03 +0530

    refactor(back-end): Delete unnecassary imports and route handles

--- server/routes.js
+++ server/routes.js
@@ -1,16 +1,14 @@
 const express=require('express');
 const { addDoctor } = require('./controllers/DoctorController');
-const { addClinicSessionDetail } = require('./controllers/ClinicSessionDetailController');
 const { addRoom } = require('./controllers/RoomController');
-const ClinicSession = require('./models/ClinicSession');
-const { addClinicSession } = require('./controllers/ClinicSessionController');
+
 const router=express.Router();
 
 router.post('/doctors', addDoctor);
-router.post('/clinicSessionDetails', addClinicSessionDetail);
+
 
 router.post('/rooms',addRoom);
-router.post('/clinicSessions',addClinicSession);
+
 
 
 module.exports=router;
