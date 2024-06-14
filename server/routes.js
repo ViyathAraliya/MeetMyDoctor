b200b07d15e92902dbbe6b141d77aaa9b80093be
viyath araliya <viyatharaliya@gmail.com>
2024-06-15 00:27:53 +0530

    feat(server-side): Add route handle for 'addAppointment' method

--- server/routes.js
+++ server/routes.js
@@ -2,6 +2,7 @@ const express=require('express');
 const { addDoctor } = require('./controllers/DoctorController');
 const { addRoom, addClinicSessionToRoom } = require('./controllers/RoomController');
 const { addClinicSession } = require('./controllers/ClinicSessionController');
+const addAppointment = require('./controllers/AppointmentController');
 
 const router=express.Router();
 
@@ -10,7 +11,8 @@ router.post('/doctors', addDoctor);
 
 router.post('/rooms',addRoom);
 router.post('/clinicSessions', addClinicSession);
-router.get('addClinicSessionsToRoom/ : roomId', addClinicSessionToRoom);
+
+router.post('/appointments', addAppointment);
 
 
 
