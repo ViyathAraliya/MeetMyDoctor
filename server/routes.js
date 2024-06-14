8e3c001b1e2e72703cbb69959139ecd50568987c
viyath araliya <viyatharaliya@gmail.com>
2024-06-14 11:00:22 +0530

    feat(server-side): Add route hanlder to 'addClinicSessionTorRoom' function

--- server/routes.js
+++ server/routes.js
@@ -1,6 +1,6 @@
 const express=require('express');
 const { addDoctor } = require('./controllers/DoctorController');
-const { addRoom } = require('./controllers/RoomController');
+const { addRoom, addClinicSessionToRoom } = require('./controllers/RoomController');
 const { addClinicSession } = require('./controllers/ClinicSessionController');
 
 const router=express.Router();
@@ -10,7 +10,7 @@ router.post('/doctors', addDoctor);
 
 router.post('/rooms',addRoom);
 router.post('/clinicSessions', addClinicSession);
-router.get('clinicSessions/ : roomDd', addClinicSession);
+router.get('addClinicSessionsToRoom/ : roomId', addClinicSessionToRoom);
 
 
 
