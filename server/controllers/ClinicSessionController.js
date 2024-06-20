496d5b5394da7e8da32a7e563f54dfd642f22581
viyath araliya <viyatharaliya@gmail.com>
2024-06-20 14:57:05 +0530

    Fix(server-side) : Correct time slot overlapping prevention logic in 'addClinicSession' method at 'ClinicSessionController'

--- server/controllers/ClinicSessionController.js
+++ server/controllers/ClinicSessionController.js
@@ -11,54 +11,65 @@ const Doctor = require("../models/Doctor");
  */
 const addClinicSession = async (req, res) => {
 
-  //deserializing the request body
+  //step 1: creating the 'ClinicSession' object
   const { id, doctorId, startsAt, endsAt, roomId } = req.body;
-
   const clinicSessionDto = new ClinicSessionDto(id, doctorId, startsAt, endsAt, roomId);
-
   const clinicSession = new ClinicSession(clinicSessionDto);
 
 
   try {
-    //checking if time slots overlaps
+    
     const doctorID = clinicSession.doctorId;
-   
     const doctor = await Doctor.findById(doctorID);
    
-   l1: if (doctor != null) {
+    // step 2: validating that the 'Doctor' document is found
+    if(doctor==null){
+      return res.status(404).send("doctor not found");
+    }
+    
+    // step 3: retriving all 'ClinicSession' documents which belong to this doctor
       const clinicSessions = await ClinicSession.find({doctorId:doctorID});
-      console.log(clinicSessions);
-      if(clinicSession==null){break l1;
 
-      }
+    // case 1: If doctor is not registered with any clinic session , saving 'ClinicSession' without validating time slots
+     if(clinicSessions.length==0){
+      await clinicSession.save();
+      return res.status(201).send(clinicSession);
+     }
+      
+
+
+     // case 2: doctor already has clinic sessions
+     // case2 2 step 1: Time slot overlapping prevention
       for (let i = 0; i < clinicSessions.length; i++) {
         const retreivedClinicSession = clinicSessions[i];
         const retrivedDoctor = clinicSessions[i].doctorId;
-        if (retrivedDoctor.equals(doctorID)) {  //checking if doctor already has a session registered
-          const retreivedStartsAt = retreivedClinicSession.startsAt;
-          const retreivedEndsAt = retreivedClinicSession.endsAt;
-          const startsAt = clinicSession.startsAt;
-          const endsAt = clinicSession.endsAt;
+
+     
+        if (retrivedDoctor.equals(doctorID)) {  // if this clinic session belongs to this doctor
+          const retreivedStartsAt = retreivedClinicSession.startsAt;// starting tiime of already registered clinic session
+          const retreivedEndsAt = retreivedClinicSession.endsAt;// ending time of   ,,   ,, 
+          const startsAt = clinicSession.startsAt; //,,    ,,    new    ,, 
+          const endsAt = clinicSession.endsAt;// ,,     ,,    new     ,, 
 
           //ensuring timeSlots dont overlap
-          if (!((retreivedStartsAt > startsAt && retreivedEndsAt > endsAt) ||
-            (retreivedEndsAt < startsAt && retreivedEndsAt < endsAt))) {
+          if (!((startsAt <=retreivedStartsAt  &&  endsAt <= retreivedStartsAt) ||
+            (startsAt >= retreivedEndsAt && endsAt >= retreivedEndsAt))) {
           return  res.status(409).send(" time slots of the doctor overlaps");
           }
         }
       }
 
-    }
+  
 
     //
 
-    //creating clinic session
+    // case 2 step 2: saving the 'CliniSession'
     await clinicSession.save();
 
-    //retrieving 'Room' document by 'roomID'
+    //case 2 step 2: retrivingthe 'Room' document affiliated with this 'ClinicSession'
     const room = await Room.findById(clinicSession.roomId);
 
-    //pushing clinicSession reference to 'clinicSessions' array and saving
+    //case 2 step 4: pushing reference of this 'ClinicSession'  to 'clinicSessions' array in 'Room' document and saving
     room.clinicSessions.push(clinicSession._id);
     await room.save();
 
