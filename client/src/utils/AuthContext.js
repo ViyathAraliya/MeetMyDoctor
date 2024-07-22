08cf66a98f44a874420cc4a174219149c638b2f3
viyath araliya <viyatharaliya@gmail.com>
2024-07-22 13:30:44 +0530

    fix(client-side): Ensure loginDetails persistence in AuthContext using JSON.stringify
    Updated login function in AuthContext to correctly stringify loginDetails before storing in localStorage. Adjusted useEffect to parse loginDetails accurately on component mount, preventing null reset issue on page refresh.

--- client/src/utils/AuthContext.js
+++ client/src/utils/AuthContext.js
@@ -1,42 +1,49 @@
-import { createContext,useContext,useEffect,useState }  from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
-const AuthContext=createContext({
-    isAutheticated: false,
-    loginDetails:null,
-    login:()=>{},
-    logout:()=>{}
+const AuthContext = createContext({
+    isAuthenticated: false,
+    loginDetails: null,
+    login: () => {},
+    logout: () => {}
 });
 
-export const AuthProvider=({children})=>{
-    const[isAuthenticated,setIsAuthenticated]=useState(false);
-    const[loginDetails, setLoginDetails]=useState(null);
+export const AuthProvider = ({ children }) => {
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [loginDetails, setLoginDetails] = useState(null);
 
-    const login=(data)=>{
-        console.log(data.loginDetails)
+    const login = (data) => {
+        console.log(data);
         setIsAuthenticated(true);
-       setLoginDetails(data);
-        localStorage.setItem("loginDetails",data);
-    }
+        setLoginDetails(data);
+        localStorage.setItem("loginDetails", JSON.stringify(data));
+    };
 
-    const logout=()=>{
+    const logout = () => {
         setIsAuthenticated(false);
         setLoginDetails(null);
         localStorage.removeItem("loginDetails");
-    }
+    };
 
-    useEffect(()=>{
-        const login_Details=localStorage.getItem("loginDetails");
-        if(login_Details){
-            setIsAuthenticated(true);
-            setLoginDetails(loginDetails);
+    useEffect(() => {
+        const login_Details = localStorage.getItem("loginDetails");
+        if (login_Details) {
+            try {
+                const parsedDetails = JSON.parse(login_Details);
+                setIsAuthenticated(true);
+                setLoginDetails(parsedDetails);
+            } catch (error) {
+                console.error("Error parsing login details from localStorage", error);
+            }
         }
-    },[])
+    }, []);
 
-    return(<AuthContext.Provider value={{isAuthenticated,loginDetails,login, logout}}>
-        {children}
-        </AuthContext.Provider>);
-}
+    return (
+        <AuthContext.Provider value={{ isAuthenticated, loginDetails, login, logout }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
 
-export const useAuth=()=>{
+export const useAuth = () => {
     return useContext(AuthContext);
-}
+};
